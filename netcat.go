81aec94cd9a36f302c5e08f672a753c803aec2b5
Pavel Derendyaev <paul@reic.ru>
2014-06-23 12:29:01 +0400

    Program terminates after remote side is closed

--- netcat.go
+++ netcat.go
@@ -7,24 +7,30 @@ import (
 	"os"
 )
 
-func handle(r io.Reader, w io.Writer) {
-	defer func() {
-		if con, ok := w.(net.Conn); ok {
-			con.Close();
-			log.Printf("Connection from %v is closed\n", con.RemoteAddr())
+func handle(r io.Reader, w io.Writer) <-chan bool {
+	buf := make([]byte, 1024)
+	c := make(chan bool)
+	go func() {
+		defer func() {
+			if con, ok := w.(net.Conn); ok {
+				con.Close();
+				log.Printf("Connection from %v is closed\n", con.RemoteAddr())
+			}
+			c <- true
+		}()
+
+		for {
+			n, err := r.Read(buf)
+			if err != nil {
+				if err != io.EOF {
+					log.Printf("Read error: %s\n", err)
+				}
+				break
+			}
+			w.Write(buf[0:n])
 		}
 	}()
-	buf := make([]byte, 1024)
-	for {
-		n, err := r.Read(buf)
-		if err != nil {
-			if err != io.EOF {
-				log.Printf("Read error: %s\n", err)
-			}
-			break
-		}
-		w.Write(buf[0:n])
-	}
+	return c
 }
 
 func main() {
@@ -33,14 +39,20 @@ func main() {
 		log.Println(err)
 		return
 	}
-	for {
-		con, err := ln.Accept()
-		log.Println("Connect from", con.RemoteAddr())
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		go handle(os.Stdin, con)
-		go handle(con, os.Stdout)
+
+	con, err := ln.Accept()
+	log.Println("Connect from", con.RemoteAddr())
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	c1 := handle(os.Stdin, con)
+	c2 := handle(con, os.Stdout)
+	select {
+	case <-c1:
+		log.Println("Local program is terminated")
+	case <-c2:
+		log.Println("Remote connection is closed")
 	}
 }
