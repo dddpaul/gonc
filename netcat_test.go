0427f06a959833267979d880d33fefabbfffd732
Pavel Derendyaev <paul@reic.ru>
2014-08-22 17:59:10 +0400

    Use idiomatic repetitive error handling technique

--- netcat_test.go
+++ netcat_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 	"strings"
+	"errors"
 )
 
 var Host = "localhost"
@@ -12,38 +13,60 @@ var Port = ":9999"
 var Input = "Input from other side, пока, £, 语汉"
 
 func TestListen(t *testing.T) {
-	// Send test data to listener from goroutine
+	err := testListen(t)
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func testListen(t *testing.T) error {
+	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
+	ec := make(chan error)
 	go func() {
-		time.Sleep( 500 * time.Millisecond )
+		// Wait for main thread starts listener
+		time.Sleep( 200 * time.Millisecond )
 		con, err := net.Dial("tcp", Host+Port)
 		if( err != nil ) {
-			t.Error(err)
-			return
+			ec<-err
 		}
-		c := handle(strings.NewReader(Input), con)
-		if !<-c {
-			t.Error("handle() must write to result channel")
+
+		// Transfer data
+		c1 := handle(strings.NewReader(Input), con)
+
+		// Wait for data will be transferred
+		time.Sleep( 200 * time.Millisecond )
+		select {
+		case <-c1:
+		default:
+			ec<-errors.New("handle() must write to result channel")
 		}
+	    ec<-nil
 	}()
 
 	ln, err := net.Listen("tcp", Port)
 	if err != nil {
-		t.Error(err)
-		return
+		return err
 	}
+
 	con, err := ln.Accept()
 	if err != nil {
-		t.Error(err)
-		return
+		return err
 	}
+
 	buf := make([]byte, 1024)
 	n, err := con.Read(buf)
 	if err != nil {
-		t.Error(err)
-		return
+		return err
 	}
+
 	if string(buf[0:n]) != Input {
-		t.Error("Received string is invalid")
-		return
+		return errors.New("Received string is invalid")
 	}
+
+	// Wait for errors from goroutine
+	if err := <-ec; err != nil {
+		return err
+	}
+
+	return nil
 }
