2d07443978d4d13ebb547b348cb4439bbf750792
Pavel Derendyaev <paul@reic.ru>
2014-09-04 16:12:47 +0400

    Rewrite test with testify package

--- netcat_test.go
+++ netcat_test.go
@@ -5,122 +5,72 @@ import (
 	"testing"
 	"time"
 	"strings"
-	"errors"
+	"github.com/stretchr/testify/assert"
 )
 
-var Host = "localhost"
-var Port = ":9999"
+var Host = "127.0.0.1"
+var Port = ":9991"
 var Input = "Input from other side, пока, £, 语汉"
 
 func TestTCP(t *testing.T) {
-	err := testTCP(t)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func TestUDP(t *testing.T) {
-	err := testUDP(t)
-	if err != nil {
-		t.Error(err)
-	}
-}
-
-func testTCP(t *testing.T) error {
 	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
-	ec := make(chan error)
 	go func() {
 		// Wait for main thread starts listener
 		time.Sleep( 200 * time.Millisecond )
 		con, err := net.Dial("tcp", Host+Port)
-		if( err != nil ) {
-			ec<-err
-		}
+		assert.Nil(t, err)
 
 		// Transfer data
-		c1 := handle(strings.NewReader(Input), con)
+		c1 := readAndWrite(strings.NewReader(Input), con)
 
 		// Wait for data will be transferred
 		time.Sleep( 200 * time.Millisecond )
 		select {
 		case <-c1:
 		default:
-			ec<-errors.New("handle() must write to result channel")
+			t.Fatal("handle() must write to result channel")
 		}
-	    ec<-nil
 	}()
 
 	ln, err := net.Listen("tcp", Port)
-	if err != nil {
-		return err
-	}
+	assert.Nil(t, err)
 
 	con, err := ln.Accept()
-	if err != nil {
-		return err
-	}
+	assert.Nil(t, err)
 
 	buf := make([]byte, 1024)
 	n, err := con.Read(buf)
-	if err != nil {
-		return err
-	}
+	assert.Nil(t, err)
 
-	if string(buf[0:n]) != Input {
-		return errors.New("Received string is invalid")
-	}
-
-	// Wait for errors from goroutine
-	if err := <-ec; err != nil {
-		return err
-	}
-
-	return nil
+	assert.Equal(t, Input, string(buf[0:n]))
 }
 
-func testUDP(t *testing.T) error {
+func TestUDP(t *testing.T) {
 	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
-	ec := make(chan error)
 	go func() {
 		// Wait for main thread starts listener
 		time.Sleep( 200 * time.Millisecond )
 		con, err := net.Dial("udp", Host+Port)
-		if( err != nil ) {
-			ec<-err
-		}
+		assert.Nil(t, err)
 
 		// Transfer data
-		c1 := handle(strings.NewReader(Input), con)
+		c1 := readAndWrite(strings.NewReader(Input), con)
 
 		// Wait for data will be transferred
 		time.Sleep( 200 * time.Millisecond )
 		select {
 		case <-c1:
 		default:
-		ec<-errors.New("handle() must write to result channel")
+			t.Fatal("handle() must write to result channel")
 		}
-		ec<-nil
 	}()
 
 	con, err := net.ListenPacket("udp", Port)
-	if err != nil {
-		return err
-	}
+	assert.Nil(t, err)
 
 	buf := make([]byte, 1024)
 	n, _, err := con.ReadFrom(buf)
-	if err != nil {
-		return err
-	}
+	assert.Nil(t, err)
 
-	if string(buf[0:n]) != Input {
-		return errors.New("Received string is invalid")
-	}
-
-	// Wait for errors from goroutine
-	if err := <-ec; err != nil {
-		return err
-	}
-
-	return nil
+	assert.Equal(t, Input, string(buf[0:n]))
 }
