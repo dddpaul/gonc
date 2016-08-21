4763bb464699cd7f32818ea1c6d28d6bc40a161e
Pavel Derendyaev <paul@reic.ru>
2016-08-21 13:55:16 +0300

    Call TransferStreams from test

--- netcat_test.go
+++ netcat_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -28,6 +29,20 @@ func (rc ReadCloser) Close() error {
 }
 
 func TestTCP(t *testing.T) {
+	// Capture STDIN and STDOUT streams
+	oldStdin := os.Stdin
+	oldStdout := os.Stdout
+
+	// Bytes written to w1 are read from os.Stdin
+	r1, w1, e := os.Pipe()
+	assert.Nil(t, e)
+	os.Stdin = r1
+
+	// Bytes written os.Stdout are read from r2
+	// r2, w2, e := os.Pipe()
+	// assert.Nil(t, e)
+	// os.Stdout = w2
+
 	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
 	go func() {
 		// Wait for main thread starts listener
@@ -36,16 +51,12 @@ func TestTCP(t *testing.T) {
 		assert.Nil(t, err)
 
 		// Transfer data
-		c1 := copyStreams(ReadCloser{r: strings.NewReader(Input)}, con)
+		_, err = w1.Write([]byte(Input))
+		assert.Nil(t, err)
+		TransferStreams(con)
 
 		// Wait for data will be transferred
 		time.Sleep(200 * time.Millisecond)
-		select {
-		case progress := <-c1:
-			t.Logf("Remote connection is closed: %+v\n", progress)
-		default:
-			t.Fatal("handle() must write to result channel")
-		}
 	}()
 
 	ln, err := net.Listen("tcp", Port)
@@ -57,6 +68,10 @@ func TestTCP(t *testing.T) {
 	buf := make([]byte, 1024)
 	n, err := con.Read(buf)
 	assert.Nil(t, err)
+
+	// Restore STDIN and STDOUT
+	os.Stdin = oldStdin
+	os.Stdout = oldStdout
 
 	assert.Equal(t, Input, string(buf[0:n]))
 }
