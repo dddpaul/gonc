c0a8d5821972aa92bcac527115d06fb73d84f679
Pavel Derendyaev <paul@reic.ru>
2016-08-21 22:13:05 +0300

    Call TransferPackets from test

--- netcat_test.go
+++ netcat_test.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"net"
 	"os"
-	"strings"
 	"testing"
 	"time"
 
@@ -15,50 +13,28 @@ var Host = "127.0.0.1"
 var Port = ":9991"
 var Input = "Input from other side, пока, £, 语汉"
 
-// ReadCloser is used to wrap strings.Reader for implementing io.ReadCloser interface
-type ReadCloser struct {
-	r *strings.Reader
-}
-
-func (rc ReadCloser) Read(p []byte) (n int, err error) {
-	return rc.r.Read(p)
-}
-
-func (rc ReadCloser) Close() error {
-	return nil
-}
-
-func TestTCP(t *testing.T) {
-	// Capture STDIN and STDOUT streams
+func TestTransferStreams(t *testing.T) {
 	oldStdin := os.Stdin
-	oldStdout := os.Stdout
 
 	// Bytes written to w1 are read from os.Stdin
-	r1, w1, e := os.Pipe()
+	r, w, e := os.Pipe()
 	assert.Nil(t, e)
-	os.Stdin = r1
+	os.Stdin = r
 
-	// Bytes written os.Stdout are read from r2
-	// r2, w2, e := os.Pipe()
-	// assert.Nil(t, e)
-	// os.Stdout = w2
-
-	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
+	// Send data to server from goroutine and wait for potentials errors at the end of the test
 	go func() {
-		// Wait for main thread starts listener
+		// Wait for main thread starts server
 		time.Sleep(200 * time.Millisecond)
 		con, err := net.Dial("tcp", Host+Port)
 		assert.Nil(t, err)
 
-		// Transfer data
-		_, err = w1.Write([]byte(Input))
+		// Client sends data
+		_, err = w.Write([]byte(Input))
 		assert.Nil(t, err)
 		TransferStreams(con)
-
-		// Wait for data will be transferred
-		time.Sleep(200 * time.Millisecond)
 	}()
 
+	// Server receives data
 	ln, err := net.Listen("tcp", Port)
 	assert.Nil(t, err)
 
@@ -69,35 +45,30 @@ func TestTCP(t *testing.T) {
 	n, err := con.Read(buf)
 	assert.Nil(t, err)
 
-	// Restore STDIN and STDOUT
-	os.Stdin = oldStdin
-	os.Stdout = oldStdout
-
 	assert.Equal(t, Input, string(buf[0:n]))
+
+	os.Stdin = oldStdin
 }
 
-func TestUDP(t *testing.T) {
-	// Send test data to listener from goroutine and wait for potentials errors at the end of the test
+func TestTransferPackets(t *testing.T) {
+	oldStdin := os.Stdin
+
+	// Bytes written to w1 are read from os.Stdin
+	r, w, e := os.Pipe()
+	assert.Nil(t, e)
+	os.Stdin = r
+
+	// Send test data to server from goroutine and wait for potentials errors at the end of the test
 	go func() {
-		// Wait for main thread starts listener
+		// Wait for main thread starts server
 		time.Sleep(200 * time.Millisecond)
 		con, err := net.Dial("udp", Host+Port)
 		assert.Nil(t, err)
 
-		// Transfer data
-		addr, err := net.ResolveUDPAddr("udp", Host+Port)
+		// Client sends data
+		_, err = w.Write([]byte(Input))
 		assert.Nil(t, err)
-		fmt.Println(con.RemoteAddr())
-		c1 := copyPackets(ReadCloser{r: strings.NewReader(Input)}, con, addr)
-
-		// Wait for data will be transferred
-		time.Sleep(200 * time.Millisecond)
-		select {
-		case progress := <-c1:
-			t.Logf("Remote connection is closed: %+v\n", progress)
-		default:
-			t.Fatal("handle() must write to result channel")
-		}
+		TransferStreams(con)
 	}()
 
 	con, err := net.ListenPacket("udp", Port)
@@ -108,4 +79,6 @@ func TestUDP(t *testing.T) {
 	assert.Nil(t, err)
 
 	assert.Equal(t, Input, string(buf[0:n]))
+
+	os.Stdin = oldStdin
 }
